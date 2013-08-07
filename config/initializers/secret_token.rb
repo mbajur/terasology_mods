a8c24ad31738a626d0a7c1890d33dc92440f1a61
Mike Bajur <mbajur@gmail.com>
2013-08-07 11:36:27 +0200

    Init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TerasologyMods::Application.config.secret_token = '3a2883f5a8002bf6cab41ac7c6c0e9ab19ebb9c9eef802d176481f4ed9c732faccd56ec56196da43bf0e6980c4d487ea77070621b70d48385fcfc7ea07be3042'
