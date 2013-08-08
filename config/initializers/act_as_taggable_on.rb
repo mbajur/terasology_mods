6737ea3c09a683ffd3495741f91dfa8dc645c430
Mike Bajur <mbajur@gmail.com>
2013-08-08 10:02:17 +0200

    Remove friendly_id

--- config/initializers/act_as_taggable_on.rb
+++
@@ -1,6 +0,0 @@
-ActsAsTaggableOn::Tag.class_eval do
-  has_friendly_id :name,
-                  :use_slug => true,
-                  :approximate_ascii => true,
-                  :reserved_words => ['show', 'edit', 'create', 'update', 'destroy']
-end
